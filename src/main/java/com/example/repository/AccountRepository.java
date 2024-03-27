ba52b15e230f038ad534f8306ad49ad91dc5db41
Veronie Cazeau <veronie.cazeau@gmail.com>
2024-03-27 02:15:50 +0000

    Added methods into MessageService; added exceptions file

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,5 @@
 package com.example.repository;
 
 public interface AccountRepository {
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,52 @@
 package com.example.service;
 
+import com.example.entity.Message;
+import com.example.exception.ResourceNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class MessageService {
+
+    private List<Message> messages = new ArrayList<>();
+    
+    // #4 retrieve all messages.
+    public List <Message> getAllMessages() {
+        return messages;
+    }
+
+    // #5 retrieve a message by its ID.
+    public Message getMessageById(int message_id) throws ResourceNotFoundException {
+        for (Message message:messages) {
+            if (message.getMessage_id().equals(message_id)) {
+                return message;
+            }
+        }
+        throw new ResourceNotFoundException(message_id + "was not found.");
+    }
+
+    // #6 delete a message identified by a message ID.
+    public void deleteMessage(int message_id) {
+        messages.removeIf(message -> message.getMessage_id().equals(message_id));
+    }
+
+    // #7 update a message text identified by a message ID. (PATCH)
+    public Message updateMessage(Integer message_id, Integer posted_by, String message_text, Long time_posted_epoch) throws ResourceNotFoundException {
+        for (Message message:messages) {
+            if (message.getMessage_id().equals(message_id)){
+                if (message_text.length() >= 255 && !message_text.isBlank());
+                return message;
+            }
+        }
+        throw new ResourceNotFoundException(message_id + "was not found. Please check id provided or try another.");
+
+    }
+
+    // // #8 retrieve all messages written by a particular user.
+    // public List<Message> getAllMessagesByUser() {
+
+    // }
+
 }
